4ab0a15d5058322f62af5987e83a38c6b5f3f039
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-01 03:35:12 +0430

    answering to the questions

--- queries.sql
+++ queries.sql
@@ -9,3 +9,49 @@ SELECT name,escape_attempts FROM animals WHERE weight_kg>10.5;
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name!='Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <=17.3;
+
+/* Updating and Use of TRANSACTIONS*/
+
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals
+
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+COMMIT;
+
+
+BEGIN;
+DELETE FROM animals where date_of_birth > '2022-01-01';
+COMMIT;
+
+BEGIN;
+SAVEPOINT save1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK ;
+SELECT * FROM animals;
+COMMIT;
+
+BEGIN;
+UPDATE animals SET weight_kg =weight_kg*-1 WHERE weight_ks IS <0;
+COMMIT;
+
+/*Answering the Questions*/
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
+SELECT AVG(weight_kg) AS average_weight FROM animals ;
+SELECT * FROM animals ORDER BY escape_attempts DESC LIMIT 1;
+SELECT * FROm animals ORDER BY neutered DESC LIMIT 7;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
