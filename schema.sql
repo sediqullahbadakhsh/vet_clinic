d787e4ebcaaacecaff0ccf85e19b379b2d583025
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-01 03:11:45 +0430

    updat and use of Transaction

--- schema.sql
+++ schema.sql
@@ -12,3 +12,32 @@ CREATE TABLE animals(
 
 /* Add species column*/
 ALTER TABLE animals ADD species VARCHAR(250);
+
+/* Updating and Use of TRANSACTIONS*/
+
+ALTER TABLE animals ALTER COLUMN species UNDEFINED;
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+COMMIT;
+
+
+BEGIN;
+DELETE FROM animals where date_of_birth > '2022-01-01';
+COMMIT;
+
+BEGIN;
+SAVEPOINT save1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK ;
+SELECT * FROM animals;
+COMMIT;
+
+BEGIN;
+UPDATE animals SET weight_kg =weight_kg*-1 WHERE weight_ks IS <0;
+COMMIT;
