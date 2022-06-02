eaffd3b5c99223a06128d0a50b8afd9a20436f0e
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-03 01:39:15 +0430

    creating tables and modifying animals table

--- schema.sql
+++ schema.sql
@@ -13,4 +13,43 @@ CREATE TABLE animals(
 /* Add species column*/
 ALTER TABLE animals ADD species VARCHAR(250);
 
+/* Creating Owners Table*/
+
+CREATE TABLE
+  owners (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(250),
+    age INT
+   
+  );
+  /*Creating Species Table*/
+
+CREATE TABLE
+  species (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    NAME VARCHAR(250)
+   
+  );
+
+  /*Modifying animals Table*/
+
+ALTER TABLE animals DROP COLUMN IF EXISTS species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD
+  CONSTRAINT species_fk FOREIGN KEY (species_id) REFERENCES species(id)
+  ON
+DELETE CASCADE;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD
+  CONSTRAINT owner_fk FOREIGN KEY (owner_id) REFERENCES owners(id)
+  ON
+DELETE CASCADE;
 
